fba025848c2088d80cbb3eb4fa28e9db8390f7ba
TharHtet236401 <tharhteta3@gmail.com>
2025-04-30 10:09:16 +0700

    Add projects and tasks views with corresponding URL routes; update home view for partial rendering

--- base/views.py
+++ base/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    if request.headers.get('HX-Request'):
+        return render(request, 'base/partials/home-partial.html')
     return render(request, 'base/home.html')
+
+def projects(request):
+    return render(request, 'base/projects.html')
+
+def tasks(request):
+    return render(request, 'base/tasks.html')
+
+
