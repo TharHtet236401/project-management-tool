b4e0eb28d0d4f41c0add6912a8a3211e870025e6
TharHtet236401 <tharhteta3@gmail.com>
2025-04-30 10:43:43 +0700

    Refactor home view to remove partial rendering and update main content structure; adjust button target in projects.html and add id to base container in main.html

--- base/views.py
+++ base/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    if request.headers.get('HX-Request'):
-        return render(request, 'base/partials/home-partial.html')
     return render(request, 'base/home.html')
 
 def projects(request):
