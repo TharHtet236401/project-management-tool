8af734d8a33cc89efb8f206d3bc384d1e483fed0
TharHtet236401 <tharhteta3@gmail.com>
2025-05-03 09:49:15 +0700

    Add Profile and Project models with corresponding admin registration; update projects view to fetch all projects and modify projects.html to include project list template.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roles = [
+        ('manager', 'Manager'),
+        ('developer', 'Developer'),
+        ('designer', 'Designer'),
+    ]
+    role = models.CharField(max_length=20, choices=roles)
+    phone = models.CharField(max_length=20, blank=True)
+    projects_assigned = models.ManyToManyField('Project', blank=True)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.role
+    
+    def projects_count(self):
+        return self.projects_assigned.count()
+
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+
--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
 
 def home(request):
     return render(request, 'base/home.html')
 
 def projects(request):
-    return render(request, 'base/projects.html')
+    projects = Project.objects.all()
+    return render(request, 'base/projects.html', {'projects': projects})
 
 def tasks(request):
     return render(request, 'base/tasks.html')
